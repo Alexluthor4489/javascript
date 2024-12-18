296ac27c598a940622f7a3be6bdb5c79842f8ce8
Alexluthor4489 <145698841+Alexluthor4489@users.noreply.github.com>
2024-12-18 06:21:53 -0300

    exercicio 1

--- modulo-d/switch-case-01.js
+++ modulo-d/switch-case-01.js
@@ -1,26 +1,26 @@
 let agora = new Date()//Esta é uma forma de pegar o horário atualizado do seu sistema
-let diaSem = agora.getDay()//Aqui estamos pegando a funcionalidade dias da semana.
+let diaSem = agora.getDay()//Aqui estamos pegando a funcionalidade dias da semana. O getDay trata cada dia da semana como um número ex: Domingo = 0, Segunda = 1, Terça = 2 e assim por diante.
 
-switch(diaSem){
-    case 0:
+switch(diaSem){//O switch case é uma estrutura mais simplicada. Ele só funciona com números comuns e strins. Ele não é tão específico quanto o if, mas em situações pontuais e simples como os dias da semana por exemplo e ajuda muito.
+    case 0://Caso seja Domingo
         console.log('Hoje é domingo')
     break;
-    case 1:
+    case 1://Caso seja Segunda
         console.log('Hoje é Segunda')
     break;
-    case 1:
+    case 2://Caso seja Terça
         console.log('Hoje é Terça')
     break;
-    case 1:
+    case 3://Caso seja Quarta
         console.log('Hoje é Quarta')
     break;
-    case 1:
+    case 4://Caso seja Quinta
         console.log('Hoje é Quinta')
     break;
-    case 1:
+    case 5://Caso seja Sexta
         console.log('Hoje é Sexta')
     break;
-    case 1:
+    case 6://Caso seja Sábado
         console.log('Hoje é Sábado')
     break;
     default:
